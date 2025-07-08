03db777aa621bb675a0c622551449ddd58bd1f16
irawanrafidzufar <irawanrafidzufar@gmail.com>
2025-07-08 11:18:28 +0700

    Header udah bagus

--- component/header.tsx
+++ component/header.tsx
@@ -17,31 +17,36 @@ const Header = () => {
   }, [])
 
   return (
-    <header className=" bg-blue-100 relative overflow-hidden">
+    <nav className=" bg-blue-100 fixed h-20 top-0 w-full overflow-hidden">
       <div className={`transition-all duration-700 ease-in-out fixed z-50 p-1
         ${isScrolled
           ? 'left-4'
           : 'left-1/2 -translate-x-1/2'
         }`
       }>
-        <Link href="/">
-          <Image
-            src="/images/logo.png"
-            alt="Logo"
-            width={70}
-            height={70}
-            className="rounded-full w-full h-full object-contain"
-          />
-        </Link>
-
-        {/* <nav className="hidden md:flex space-x-6">
-          <Link href="/" className="text-gray-700 hover:text-blue-600">Home</Link>
-          <Link href="/about" className="text-gray-700 hover:text-blue-600">About</Link>
-          <Link href="/contact" className="text-gray-700 hover:text-blue-600">Contact</Link>
-        </nav> */}
-
+        <Image
+          src="/images/logo.png"
+          alt="Logo"
+          width={70}
+          height={70}
+          className="rounded-full w-full h-full object-contain"
+        />
       </div>
-    </header>
+
+      <div className={`transition-all duration-700 ease-in-out fixed right-10 top-6
+        ${isScrolled
+          ? 'opacity-100'
+          : 'opacity-0'
+        }
+        `}>
+        <ul className="flex gap-6 font-medium text-gray-700">
+          <li className="hover:text-blue-500 transition"><Link href={"#"}>Beranda</Link></li>
+          <li className="hover:text-blue-500 transition"><Link href={"#"}>Tentang</Link></li>
+          <li className="hover:text-blue-500 transition"><Link href={"#"}>Kontak</Link></li>
+        </ul>
+      </div>
+
+    </nav>
   )
 }
 
