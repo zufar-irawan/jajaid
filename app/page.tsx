73d775f7a402f18b7bd50a478397c1f57b482668
irawanrafidzufar <irawanrafidzufar@gmail.com>
2025-07-08 10:56:32 +0700

    header update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Header from "@/component/header"
 import Footer from "@/component/Footer";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-    <Header/>
-    <Footer/>
+      <Header />
+
+      <Footer />
     </div>
+
+
   )
 }
--- component/header.tsx
+++ component/header.tsx
@@ -1,36 +1,46 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false)
 
-    return (
-    <header className="bg-white shadow-md fixed top-0 left-0 w-full z-50">
-      <div className="container mx-auto px-4 py-4 flex items-center justify-between">
-        <Link href="/" className="text-2xl font-bold text-blue-600">
-            <Image 
-              src="/images/logo.png"
-              alt="Logo"
-              width={80}
-              height={80}
-              className="rounded-full"
-            />
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
+  return (
+    <header className=" bg-blue-100 relative overflow-hidden">
+      <div className={`transition-all duration-700 ease-in-out fixed z-50 p-1
+        ${isScrolled
+          ? 'left-4'
+          : 'left-1/2 -translate-x-1/2'
+        }`
+      }>
+        <Link href="/">
+          <Image
+            src="/images/logo.png"
+            alt="Logo"
+            width={70}
+            height={70}
+            className="rounded-full w-full h-full object-contain"
+          />
         </Link>
 
-        <nav className="hidden md:flex space-x-6">
+        {/* <nav className="hidden md:flex space-x-6">
           <Link href="/" className="text-gray-700 hover:text-blue-600">Home</Link>
           <Link href="/about" className="text-gray-700 hover:text-blue-600">About</Link>
           <Link href="/contact" className="text-gray-700 hover:text-blue-600">Contact</Link>
-        </nav>
+        </nav> */}
 
       </div>
-        <div className="md:hidden bg-white border-t border-gray-200 px-4 pb-4">
-          <Link href="/" className="block py-2 text-gray-700 hover:text-blue-600">Home</Link>
-          <Link href="/about" className="block py-2 text-gray-700 hover:text-blue-600">About</Link>
-          <Link href="/contact" className="block py-2 text-gray-700 hover:text-blue-600">Contact</Link>
-        </div>
     </header>
   )
 }
